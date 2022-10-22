9947cc3774187ff89955aee3393aa2a20eceaee6
Negresko <36646219+Negresko@users.noreply.github.com>
2022-10-22 01:07:55 -0300

    Projeto REACT imersão (NLW Rocketseat)

---
+++ src/components/CreateAdBanner.tsx
@@ -0,0 +1,19 @@
+import {Headset} from 'phosphor-react'
+import * as Dialog from '@radix-ui/react-dialog'
+
+export function CreateAdBanner(){
+    return(
+        <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block">Procurando alguem para se <span className="text-transparent bg-nlw-gradient bg-clip-text">Divertir?</span></strong>
+            <span className="text-zinc-400 block">Entre no Discord! QUALQUER COISA VAI NA CASA DO <a href="" className="text-transparent bg-clip-text bg-violet-500">ROCK</a></span>
+          </div>  
+          <Dialog.Trigger className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <Headset size={24}/>
+            Entrar Agora!
+          </Dialog.Trigger>
+        </div>
+      </div>
+    )
+}
